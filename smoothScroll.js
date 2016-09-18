e89cf90ed3467002b9b990f1179e351df0476e1a
Tushar Tuteja <ttuteja@gmail.com>
2016-09-18 13:58:18 +0530

    better scroll navigation

--- smoothScroll.js
+++ smoothScroll.js
@@ -2,42 +2,39 @@ var smooth_scroll = (function() {
 
     var intervalId;
     var header_height;
-    var detla;
+    var delta;
+    var destinationY;
+    var currentY;
 
     var smoothScroll = function() {
-
-
-        destinationY = this.offsetTop - header_height;
-        currentY = window.scrollY;
-
-        if (currentY < destinationY) {
-            if (destinationY - currentY < delta) {
-                window.scrollTo(0, destinationY);
-                clearInterval(intervalId);
-
-            }
-            window.scrollTo(0, currentY + delta);
-
-        } else {
-            if (currentY - destinationY < delta) {
-                window.scrollTo(0, destinationY);
-                clearInterval(intervalId);
-            }
-            window.scrollTo(0, currentY - delta);
+		if (delta > 0  && currentY + delta > destinationY ) {
+        	delta = destinationY - currentY;
+            clearInterval(intervalId);
         }
 
-        delta = delta + 0.2;
+        if (delta < 0 && currentY + delta < destinationY){
+        	delta = destinationY - currentY;
+        	clearInterval(intervalId);
+        }
 
-
-
+        window.scrollBy(0, delta);  
+  		currentY = currentY + delta;
+        delta = delta * 1.1;
     };
 
     var scrollTo = function(el) {
         if (intervalId !== undefined || intervalId !== null) {
             clearInterval(intervalId);
         }
+       
+        destinationY = el.offsetTop - header_height;
+        currentY = window.scrollY;
+
         intervalId = setInterval(smoothScroll.bind(el), 10);
         delta = 1;
+        if (currentY > destinationY){
+    		delta = delta * -1;
+    	}
     };
 
 
@@ -51,7 +48,6 @@ var smooth_scroll = (function() {
 
     };
 
-
     var init = function(config) {
         //Get all internal links
         var internal_links = document.querySelectorAll("a[href^='#']");
@@ -62,14 +58,9 @@ var smooth_scroll = (function() {
         if (config !== undefined && config !== null) {
             if (config.header_id !== undefined && config.header_id !== null) {
                 header_height = document.getElementById(config.header_id).getBoundingClientRect().height;
-
             }
-
         }
         header_height = header_height || 0;
-
-
-
     };
 
     return {
