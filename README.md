9b79bf17df5351b1211069f3e6a4e489c7fd0a4e
Tushar Tuteja <ttuteja@gmail.com>
2016-09-18 12:41:01 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# SmoothScrollJs
-Smooth Scroll for all internal links. 
+All internal links would scroll Smoothly
+Usage
+Add this script just before the body.
+
+```
+  <script type="text/javascript">
+        smooth_scroll.init({
+            header_id : "page-header"
+        });
+    </script>
+
+
+```
+
---
+++ smoothScroll.js
@@ -0,0 +1,77 @@
+var smooth_scroll = (function(){
+
+	var intervalId;
+	var header_height;
+
+	var smoothScroll = function(){
+
+
+		destinationY = this.offsetTop - header_height;
+		currentY = window.scrollY;	
+
+		if(currentY < destinationY){
+			if (destinationY - currentY < delta){
+				window.scrollTo(0,destinationY);
+				clearInterval(intervalId);
+
+			}
+			window.scrollTo(0,currentY + delta);
+
+		}
+		else {
+			if (currentY - destinationY < delta){
+				window.scrollTo(0,destinationY);
+				clearInterval(intervalId);
+			}
+			window.scrollTo(0,currentY - delta);
+		}
+
+		delta = delta + 0.2;
+		
+	
+
+	};
+
+	var scrollTo = function(el){
+		if(intervalId !== undefined || intervalId !== null){
+			clearInterval(intervalId);
+		}
+		intervalId = setInterval(smoothScroll.bind(el),10);
+		delta = 1;
+	};
+
+
+	var onClickAnchor = function(e){
+		e.preventDefault();
+		var anchor_tag = this;
+		anchor_href = this.getAttribute("href")
+		element_id = anchor_href.slice(1,anchor_href.length);
+		var el = document.getElementById(element_id);
+		scrollTo(el);
+
+	};
+
+
+	var init =  function(config){
+		//Get all internal links
+		var internal_links = document.querySelectorAll("a[href^='#']");
+		for (var i = 0; i < internal_links.length ; i++ ){
+			link = internal_links[i];
+			link.addEventListener("click",onClickAnchor.bind(link));
+		}
+		if(config !== undefined && config !== null ){
+			if(config.header_id !== undefined && config.header_id !==null){
+				header_height = document.getElementById(config.header_id).getBoundingClientRect().height;
+
+			}
+
+		}
+
+
+	};
+
+	return {
+		init:init
+	}
+
+})();
