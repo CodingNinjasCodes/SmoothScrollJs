b3bf700a762cf0fcde2a5f4bc74a2353b1c376d0
Tushar Tuteja <ttuteja@gmail.com>
2016-09-20 01:00:28 +0530

    Added ignore links option to config

--- smoothScroll.js
+++ smoothScroll.js
@@ -5,20 +5,25 @@ var smooth_scroll = (function() {
     var delta;
     var destinationY;
     var currentY;
+    var ignore_links;
+
+    var isInArray = function(value, array) {
+        return array.indexOf(value) > -1;
+    }
 
     var smoothScroll = function() {
-		if (delta > 0  && currentY + delta > destinationY ) {
-        	delta = destinationY - currentY;
+        if (delta > 0 && currentY + delta > destinationY) {
+            delta = destinationY - currentY;
             clearInterval(intervalId);
         }
 
-        if (delta < 0 && currentY + delta < destinationY){
-        	delta = destinationY - currentY;
-        	clearInterval(intervalId);
+        if (delta < 0 && currentY + delta < destinationY) {
+            delta = destinationY - currentY;
+            clearInterval(intervalId);
         }
 
-        window.scrollBy(0, delta);  
-  		currentY = currentY + delta;
+        window.scrollBy(0, delta);
+        currentY = currentY + delta;
         delta = delta * 1.1;
     };
 
@@ -26,15 +31,15 @@ var smooth_scroll = (function() {
         if (intervalId !== undefined || intervalId !== null) {
             clearInterval(intervalId);
         }
-       
+
         destinationY = el.offsetTop - header_height;
         currentY = window.scrollY;
 
         intervalId = setInterval(smoothScroll.bind(el), 10);
         delta = 1;
-        if (currentY > destinationY){
-    		delta = delta * -1;
-    	}
+        if (currentY > destinationY) {
+            delta = delta * -1;
+        }
     };
 
 
@@ -50,16 +55,24 @@ var smooth_scroll = (function() {
 
     var init = function(config) {
         //Get all internal links
-        var internal_links = document.querySelectorAll("a[href^='#']");
-        for (var i = 0; i < internal_links.length; i++) {
-            link = internal_links[i];
-            link.addEventListener("click", onClickAnchor.bind(link));
-        }
+
         if (config !== undefined && config !== null) {
             if (config.header_id !== undefined && config.header_id !== null) {
                 header_height = document.getElementById(config.header_id).getBoundingClientRect().height;
             }
+
+            ignore_links = config.ignore_links || []
         }
+
+        var internal_links = document.querySelectorAll("a[href^='#']");
+        for (var i = 0; i < internal_links.length; i++) {
+            link = internal_links[i];
+            var href = link.getAttribute("href");
+            if (href !== "#" && !isInArray(href.slice(1, href.length), ignore_links)) {
+                link.addEventListener("click", onClickAnchor.bind(link));
+            }
+        }
+
         header_height = header_height || 0;
     };
 
